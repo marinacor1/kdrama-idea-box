20ddeac2dc06473d3467c2e1c116596f47c171f8
marinacor1 <marinaspam1234@gmail.com>
2016-04-14 17:02:44 -0600

    Test for sad path without description works

--- spec/features/user_creates_a_kdrama_idea_spec.rb
+++ spec/features/user_creates_a_kdrama_idea_spec.rb
@@ -21,21 +21,35 @@ feature "user can submit kdrama idea" do
     end
   end
 
-  scenario "user cannot create without title" do
-    pitch_title = "Muderous Rampage Meets Love"
+  scenario "user cannot create idea without title" do
     pitch_description = "Lee MinHo falls in love with Anahi in the midst of a Zombie Apocalypse. Mexican and Korean love at first sight."
     visit ideas_path
     click_on "I have a bombass idea for the latest blockbuster"
 
     expect new_idea_path
     # select category, from: "title"
-    # fill_in "Title", with: pitch_title
     fill_in "Description", with: pitch_description
 
     #TODO select image from box
     click_on "Submit"
     expect new_idea_path
-    expect(page).to_not have_content pitch_title
     expect(page).to_not have_content pitch_description
+  end
+
+  scenario "user cannot create idea without description" do
+    pitch_title = "Muderous Rampage Meets Love"
+
+    visit ideas_path
+    click_on "I have a bombass idea for the latest blockbuster"
+
+    expect new_idea_path
+    # select category, from: "title"
+    fill_in "Title", with: pitch_title
+    #TODO select image from box
+    click_on "Submit"
+
+    expect new_idea_path
+
+    expect(page).to_not have_content pitch_title
   end
 end
