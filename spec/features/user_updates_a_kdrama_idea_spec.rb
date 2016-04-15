fee615cf31f57a7b10265e075eaaa909236ca7a5
marinacor1 <marinaspam1234@gmail.com>
2016-04-14 18:14:45 -0600

    working on edit test

--- spec/features/user_updates_a_kdrama_idea_spec.rb
+++ spec/features/user_updates_a_kdrama_idea_spec.rb
@@ -1,3 +1,15 @@
 require 'rails_helper'
 
-#user can 
+#idea is created
+#user visits idea show page
+#user clicks on edit link
+#user changes fields for title
+#user changes field for description
+#user clicks update idea link
+#user sees on show page the updated information
+
+feature "user can edit kdrama idea" do
+  scenario "user will
+  "
+
+end
