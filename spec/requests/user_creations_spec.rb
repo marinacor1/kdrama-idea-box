1cfabcad1089dc65dc2612a0d4fc4ca950f087f1
marinacor1 <marinaspam1234@gmail.com>
2016-04-15 11:38:43 -0600

    user creation test written

--- spec/requests/user_creations_spec.rb
+++ spec/requests/user_creations_spec.rb
@@ -2,5 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "User can be created" do
     it "use can create account" do
+      fill_in "Username", with: "marina"
+      fill_in "Password", with: "password"
+      click_button "Create Account"
+
+      expect(page).to have_content "Welcome, marina"
     end
 end
