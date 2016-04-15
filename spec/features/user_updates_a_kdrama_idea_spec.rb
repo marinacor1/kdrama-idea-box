f80013350bf68d56c2b70fbe6fabe56a4adfe30d
marinacor1 <marinaspam1234@gmail.com>
2016-04-14 18:21:18 -0600

    Update feature test created.

--- spec/features/user_updates_a_kdrama_idea_spec.rb
+++ spec/features/user_updates_a_kdrama_idea_spec.rb
@@ -9,7 +9,18 @@ require 'rails_helper'
 #user sees on show page the updated information
 
 feature "user can edit kdrama idea" do
-  scenario "user will
-  "
+  scenario "user will see the updated idea on page" do
+    new_idea = Idea.create(title: "The most amazing pitch", description: "Jude Law and Dulce Maria fall in love under the moon.")
 
+    visit idea_path(new_idea)
+
+    click_on "Edit Idea"
+
+    fill_in "Title", with: "The next best pitch"
+    fill_in "Description", with: "Dulce Maria dumped Jude Law for Ji Chang Wook"
+    click_on "Update Idea"
+
+    expect(page).to have_content "The next best pitch"
+    expect(page).to have_content  "Jude Law and Dulce Maria fall in love under the moon."
+  end
 end
