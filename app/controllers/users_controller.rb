289194e45905a09f4810194a45fcc56c751d1e8d
marinacor1 <marinaspam1234@gmail.com>
2016-04-15 12:18:20 -0600

    user can create account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(params_check)
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def params_check
+    params.require(:user).permit(:username, :password)
   end
 end
