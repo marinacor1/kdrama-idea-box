9afafc891c51bc13041b6e2df77029b352ce6735
marinacor1 <marinaspam1234@gmail.com>
2016-04-15 13:29:45 -0600

    user can login test written

--- spec/requests/user_can_logins_spec.rb
+++ spec/requests/user_can_logins_spec.rb
@@ -1,8 +1,16 @@
-# require 'rails_helper'
-#
-# RSpec.describe "user can login" do
-#   scenario "user can see user landing page" do
-#     it "" do
-#     end
-#   end
-# end
+require 'rails_helper'
+
+RSpec.describe "user can login" do
+  scenario "user can see user landing page" do
+    user = create(:user)
+
+    visit login_path
+    fill_in "Username", with: user.username
+    fill_in "Password", with: "abcd"
+    click_button "Login"
+
+    expect ideas_path
+    
+    expect(page).to have_content "Welcome, marina"
+  end
+end
--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -3,4 +3,10 @@ FactoryGirl.define do
     title "Great Idea"
     description "Description of great idea"
 end
+
+  factory :user do
+    username "marina"
+    password "abcd"
+    password_confirmation "abcd"
+  end
 end
