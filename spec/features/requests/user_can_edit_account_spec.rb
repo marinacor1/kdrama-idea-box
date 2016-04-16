45467a669085508ef467aacef0901de55100aec7
marinacor1 <marinaspam1234@gmail.com>
2016-04-16 14:47:10 -0600

    edit link working for user show page

--- spec/features/requests/user_can_edit_account_spec.rb
+++ spec/features/requests/user_can_edit_account_spec.rb
@@ -1,21 +1,20 @@
-# require 'rails_helper'
-#
-# feature 'User can edit an existing account' do
-#   include FeaturesHelper
-#   scenario 'updated account is viewable' do
-#     login_setup
-# save_and_open_page
-#     click_link "Edit Account"
-#
-#     expect(page).to have_content("Edit account")
-#
-#     fill_in "Username", with: "newuser"
-#     fill_in "Email", with: "email@email.com"
-#
-#     click_on "Update account"
-#
-#     expect(page).to have_content("Newuser")
-#     expect(page).to have_content("email@email.com")
-#
-#   end
-# end
+require 'rails_helper'
+
+feature 'User can edit an existing account' do
+  include FeaturesHelper
+  scenario 'updated account is viewable' do
+    user = create(:user)
+    login_setup
+    click_link "Edit Account"
+
+    expect(current_path).to eq(user_path(user))
+
+    fill_in "Username", with: "newuser"
+    fill_in "Email", with: "email@email.com"
+
+    click_on "Update account"
+
+    expect(page).to have_content("Newuser")
+    expect(page).to have_content("email@email.com")
+  end
+end
--- spec/features/user_creates_a_kdrama_idea_spec.rb
+++ spec/features/user_creates_a_kdrama_idea_spec.rb
@@ -1,107 +1,107 @@
-require 'rails_helper'
-
-feature "user can submit kdrama idea" do
-  xscenario "user will see idea, category and image on page" do
-    category = 'Historical Apocolyptic Comedy'
-    pitch_title = "Muderous Rampage Meets Love"
-    pitch_description = "Lee MinHo falls in love with Anahi in the midst of a Zombie Apocalypse. Mexican and Korean love at first sight."
-    user = create(:user)
-
-    visit login_path
-    fill_in "Username", with: user.username
-    fill_in "Password", with: "abcdefgh"
-    click_button "Login"
-
-    visit ideas_path(user)
-    click_on "New Idea"
-
-    expect new_idea_path(user)
-    # select category, from: "title"
-    fill_in "Title", with: pitch_title
-    fill_in "Description", with: pitch_description
-
-    #TODO select image from box
-    click_on "Submit"
-
-    expect ideas_path(user)
-
-    within("li:first") do
-      expect(page).to have_content pitch_title
-      expect(page).to have_content pitch_description
-    end
-    expect(page).to have_link "All Ideas", href: user_ideas_path(user)
-  end
-
-  xscenario "user can view previous ideas" do
-    first_idea = Idea.create(title: "The most amazing pitch", description: "Jude Law and Dulce Maria fall in love under the moon.")
-    second_idea = Idea.create(title: "The second most amazing pitch", description: "Jude Law and Anahi fall in love under the moon.")
-
-    user = create(:user)
-
-    visit login_path
-    fill_in "Username", with: user.username
-    fill_in "Password", with: "abcdefgh"
-    click_button "Login"
-
-    visit ideas_path(user)
-    within("li:first") do
-      expect(page).to have_content "The most amazing pitch"
-      expect(page).to have_content "Jude Law and Dulce Maria fall in love under the moon."
-    end
-
-    within("li:last") do
-      expect(page).to have_content "The second most amazing pitch"
-      expect(page).to have_content "Jude Law and Anahi fall in love under the moon."
-    end
-  end
-
-  xscenario "user cannot create idea without title" do
-    pitch_description = "Lee MinHo falls in love with Anahi in the midst of a Zombie Apocalypse. Mexican and Korean love at first sight."
-    user = create(:user)
-
-    visit login_path
-    fill_in "Username", with: user.username
-    fill_in "Password", with: "abcdefgh"
-    click_button "Login"
-
-    visit ideas_path(user)
-    click_on "New Idea"
-
-    expect new_idea_path(user)
-    # select category, from: "title"
-    fill_in "Description", with: pitch_description
-
-    #TODO select image from box
-    click_on "Submit"
-
-    expect(page).to have_content "Title can't be blank"
-
-    expect new_idea_path(user)
-    expect(page).to_not have_content pitch_description
-  end
-
-  xscenario "user cannot create idea without description" do
-    pitch_title = "Muderous Rampage Meets Love"
-    user = create(:user)
-
-    visit login_path
-    fill_in "Username", with: user.username
-    fill_in "Password", with: "abcdefgh"
-    click_button "Login"
-
-    visit ideas_path(user)
-    click_on "New Idea"
-
-    expect new_user_idea_path(user)
-    # select category, from: "title"
-    fill_in "Title", with: pitch_title
-    #TODO select image from box
-    click_on "Submit"
-
-    expect(page).to have_content "Description can't be blank"
-
-    expect new_idea_path(user)
-
-    expect(page).to_not have_content pitch_title
-  end
-end
+# require 'rails_helper'
+#
+# feature "user can submit kdrama idea" do
+#   xscenario "user will see idea, category and image on page" do
+#     category = 'Historical Apocolyptic Comedy'
+#     pitch_title = "Muderous Rampage Meets Love"
+#     pitch_description = "Lee MinHo falls in love with Anahi in the midst of a Zombie Apocalypse. Mexican and Korean love at first sight."
+#     user = create(:user)
+#
+#     visit login_path
+#     fill_in "Username", with: user.username
+#     fill_in "Password", with: "abcdefgh"
+#     click_button "Login"
+#
+#     visit ideas_path(user)
+#     click_on "New Idea"
+#
+#     expect new_idea_path(user)
+#     # select category, from: "title"
+#     fill_in "Title", with: pitch_title
+#     fill_in "Description", with: pitch_description
+#
+#     #TODO select image from box
+#     click_on "Submit"
+#
+#     expect ideas_path(user)
+#
+#     within("li:first") do
+#       expect(page).to have_content pitch_title
+#       expect(page).to have_content pitch_description
+#     end
+#     expect(page).to have_link "All Ideas", href: user_ideas_path(user)
+#   end
+#
+#   xscenario "user can view previous ideas" do
+#     first_idea = Idea.create(title: "The most amazing pitch", description: "Jude Law and Dulce Maria fall in love under the moon.")
+#     second_idea = Idea.create(title: "The second most amazing pitch", description: "Jude Law and Anahi fall in love under the moon.")
+#
+#     user = create(:user)
+#
+#     visit login_path
+#     fill_in "Username", with: user.username
+#     fill_in "Password", with: "abcdefgh"
+#     click_button "Login"
+#
+#     visit ideas_path(user)
+#     within("li:first") do
+#       expect(page).to have_content "The most amazing pitch"
+#       expect(page).to have_content "Jude Law and Dulce Maria fall in love under the moon."
+#     end
+#
+#     within("li:last") do
+#       expect(page).to have_content "The second most amazing pitch"
+#       expect(page).to have_content "Jude Law and Anahi fall in love under the moon."
+#     end
+#   end
+#
+#   xscenario "user cannot create idea without title" do
+#     pitch_description = "Lee MinHo falls in love with Anahi in the midst of a Zombie Apocalypse. Mexican and Korean love at first sight."
+#     user = create(:user)
+#
+#     visit login_path
+#     fill_in "Username", with: user.username
+#     fill_in "Password", with: "abcdefgh"
+#     click_button "Login"
+#
+#     visit ideas_path(user)
+#     click_on "New Idea"
+#
+#     expect new_idea_path(user)
+#     # select category, from: "title"
+#     fill_in "Description", with: pitch_description
+#
+#     #TODO select image from box
+#     click_on "Submit"
+#
+#     expect(page).to have_content "Title can't be blank"
+#
+#     expect new_idea_path(user)
+#     expect(page).to_not have_content pitch_description
+#   end
+#
+#   xscenario "user cannot create idea without description" do
+#     pitch_title = "Muderous Rampage Meets Love"
+#     user = create(:user)
+#
+#     visit login_path
+#     fill_in "Username", with: user.username
+#     fill_in "Password", with: "abcdefgh"
+#     click_button "Login"
+#
+#     visit ideas_path(user)
+#     click_on "New Idea"
+#
+#     expect new_user_idea_path(user)
+#     # select category, from: "title"
+#     fill_in "Title", with: pitch_title
+#     #TODO select image from box
+#     click_on "Submit"
+#
+#     expect(page).to have_content "Description can't be blank"
+#
+#     expect new_idea_path(user)
+#
+#     expect(page).to_not have_content pitch_title
+#   end
+# end
