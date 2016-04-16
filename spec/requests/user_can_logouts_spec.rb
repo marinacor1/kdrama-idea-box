76202e90aab32aa5d7695567a3be3ea0ce1587d8
marinacor1 <marinaspam1234@gmail.com>
2016-04-16 12:07:01 -0600

    logout passes

--- spec/requests/user_can_logouts_spec.rb
+++ spec/requests/user_can_logouts_spec.rb
@@ -1,17 +1,17 @@
-# require 'rails_helper'
-#
-# RSpec.describe "User can logout" do
-#   include Capybara::DSL
-#   scenario "user can see login page" do
-#     user = create(:user)
-#
-#     visit login_path
-#     fill_in "Username", with: user.username
-#     fill_in "Password", with: "abcdefgh"
-#     click_link "Logout"
-#
-#     expect login_path
-#
-#     expect(page).to_not have_content "Welcome, marina"
-#   end
-# end
+require 'rails_helper'
+
+RSpec.describe "User can logout" do
+  include Capybara::DSL
+  scenario "user can see login page" do
+    user = create(:user)
+
+    visit login_path
+    fill_in "Username", with: user.username
+    fill_in "Password", with: "abcdefgh"
+    click_link "Logout"
+
+    expect login_path
+
+    expect(page).to_not have_content "Welcome, marina"
+  end
+end
