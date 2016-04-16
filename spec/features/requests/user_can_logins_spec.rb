f437889b0076a859b9acdcd39437d01b6cf22e28
marinacor1 <marinaspam1234@gmail.com>
2016-04-16 14:20:25 -0600

    removing setup in other tests

--- spec/features/requests/user_can_logins_spec.rb
+++ spec/features/requests/user_can_logins_spec.rb
@@ -2,13 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "user can login" do
   include Capybara::DSL
+  include FeaturesHelper
   scenario "user can see user landing page" do
-    user = create(:user)
-
-    visit login_path
-    fill_in "Username", with: user.username
-    fill_in "Password", with: "abcdefgh"
-    click_button "Login"
+    login_setup
 
     expect(page).to have_content "Welcome, marina"
   end
