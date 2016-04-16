8d5c460eded9f7ac06daed6840d76ca54ea05856
marinacor1 <marinaspam1234@gmail.com>
2016-04-16 11:14:46 -0600

    wrapping up

--- spec/requests/user_can_logins_spec.rb
+++ spec/requests/user_can_logins_spec.rb
@@ -1,41 +1,37 @@
-# require 'rails_helper'
-#
-# RSpec.describe "user can login" do
-#   include Capybara::DSL
-#   scenario "user can see user landing page" do
-#     user = create(:user)
-#
-#     visit login_path
-#     fill_in "Username", with: user.username
-#     fill_in "Password", with: "abcdefgh"
-#     click_button "Login"
-#
-#     expect user_ideas_path(user)
-#
-#     expect(page).to have_content "Welcome, marina"
-#   end
-#
-#   scenario "user cannot see user landing page if password does not match" do
-#     user = create(:user)
-#
-#     visit login_path
-#     fill_in "Username", with: user.username
-#     fill_in "Password", with: "abcd123"
-#     click_button "Login"
-#
-#     expect user_ideas_path(user)
-#     expect(page).to_not have_content "Welcome, marina"
-#   end
-#
-#   scenario "user cannot see user landing page if username does not exist" do
-#     user = create(:user)
-#
-#     visit login_path
-#     fill_in "Username", with: "Michael Jackson"
-#     fill_in "Password", with: "abcdefgh"
-#     click_button "Login"
-#
-#     expect user_ideas_path(user)
-#     expect(page).to_not have_content "Welcome, marina"
-#   end
-# end
+require 'rails_helper'
+
+RSpec.describe "user can login" do
+  include Capybara::DSL
+  scenario "user can see user landing page" do
+    user = create(:user)
+
+    visit login_path
+    fill_in "Username", with: user.username
+    fill_in "Password", with: "abcdefgh"
+    click_button "Login"
+
+    expect(page).to have_content "Welcome, marina"
+  end
+
+  scenario "user cannot see user landing page if password does not match" do
+    user = create(:user)
+
+    visit login_path
+    fill_in "Username", with: user.username
+    fill_in "Password", with: "abcd123"
+    click_button "Login"
+
+    expect(page).to_not have_content "Welcome, marina"
+  end
+
+  scenario "user cannot see user landing page if username does not exist" do
+    user = create(:user)
+
+    visit login_path
+    fill_in "Username", with: "Michael Jackson"
+    fill_in "Password", with: "abcdefgh"
+    click_button "Login"
+
+    expect(page).to_not have_content "Welcome, marina"
+  end
+end
