5594a6f0bedfc350d51720d2c0f5d774c6768a77
marinacor1 <marinaspam1234@gmail.com>
2016-04-17 16:29:33 -0600

    user can edit idea

--- spec/features/user_updates_a_kdrama_idea_spec.rb
+++ spec/features/user_updates_a_kdrama_idea_spec.rb
@@ -1,26 +1,28 @@
-# require 'rails_helper'
-#
-# feature "user can edit kdrama idea" do
-#   scenario "user will see the updated idea on page" do
-#     user = create(:user)
-#
-#     visit login_path
-#     fill_in "Username", with: user.username
-#     fill_in "Password", with: "abcdefgh"
-#     click_button "Login"
-#
-#     new_idea = create(:idea)
-#
-#     visit idea_path(user)
-#
-#     click_on "Edit Idea"
-#     fill_in "Title", with: "The next best pitch"
-#     fill_in "Description", with: "Dulce Maria dumped Jude Law for Ji Chang Wook"
-#     click_on "Update Idea"
-#
-#     expect idea_path
-#
-#     expect(page).to have_content "The next best pitch"
-#     expect(page).to have_content  "Dulce Maria dumped Jude Law for Ji Chang Wook"
-# end
-# end
+require 'rails_helper'
+
+feature "user can edit kdrama idea" do
+  include FeaturesHelper
+  scenario "user will see the updated idea on page" do
+    user = create(:user)
+
+    login_setup
+    expect(current_path).to eq(user_path(user))
+
+    click_on "New Idea"
+    expect(current_path).to eq(new_idea_path)
+    fill_in "Title", with: "The best pitch"
+    fill_in "Description", with: "Dulce Maria dates Jude Law"
+    click_on "Submit"
+    expect(current_path).to eq(idea_path(Idea.last))
+
+    click_on "Edit Idea"
+    fill_in "Title", with: "The next best pitch"
+    fill_in "Description", with: "Dulce Maria dumped Jude Law for Ji Chang Wook"
+    click_on "Update Idea"
+
+    expect(current_path).to eq(idea_path(Idea.last))
+
+    expect(page).to have_content "The next best pitch"
+    expect(page).to have_content  "Dulce Maria dumped Jude Law for Ji Chang Wook"
+end
+end
