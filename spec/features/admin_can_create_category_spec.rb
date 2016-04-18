47243efa24385486cc89d30717bdb7a8f26c8024
marinacor1 <marinaspam1234@gmail.com>
2016-04-18 11:39:23 -0600

    All tests passing

--- spec/features/admin_can_create_category_spec.rb
+++ spec/features/admin_can_create_category_spec.rb
@@ -2,11 +2,11 @@ require "rails_helper"
 
   RSpec.describe "Logged in admin sees categories index" do
     include FeaturesHelper
-    it "Admi can see all categories" do
+    it "Admin can see all categories" do
     admin = User.create(username: "adminor",
                         password: "password",
                         role: 1)
-
+    category = Category.create(genre: "History")
     admin_login
 
     visit admin_categories_path
@@ -14,5 +14,6 @@ require "rails_helper"
     click_on "Create New Category"
 
     expect(page).to have_content "All Categories"
+    expect(page).to have_content "History"
   end
 end
